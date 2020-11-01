c2805df4debc355ed23d7a5fd1b53607e35d7445
damhung0113 <damtienhung_t62@hus.edu.vn>
2020-11-01 23:50:38 +0700

    API đăng nhập, đăng ký #13

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root "home#index"
   devise_for :users
+  as :user do
+    get "/signup", to: "devise/registrations#new"
+    post "/signup", to: "devise/registrations#create"
+    get "/login", to: "devise/sessions#new"
+    post "/login", to: "devise/sessions#create"
+    delete "/logout", to: "devise/sessions#destroy"
+  end
 end
