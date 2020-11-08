a9ecad4d674e45108dce5bed8c7709d957801998
damhung0113 <damtienhung_t62@hus.edu.vn>
2020-11-08 20:31:19 +0700

    API thêm, sửa, xóa danh mục (category) #19

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :sub_categories, dependent: :destroy, class_name: Category.name, foreign_key: :parent_id
   belongs_to :parent, class_name: Category.name, optional: true
+
+  scope :can_parent, -> { where(parent_id: nil) }
 end
